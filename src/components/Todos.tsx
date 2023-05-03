c04601a42b15df87cac5db1012cefd1232e15ebd
dzoliana <dzolicana@gmail.com>
2023-05-03 14:37:25 +0200

    Added props-dataModel branch

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,15 @@
-function Todos() {
+import React from "react";
+import { PropsWithChildren } from "react";
+import Todo from "../models/todo";
+
+const Todos: React.FC<PropsWithChildren<{ items: Todo[] }>> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
