03785fb2dae042777b392703378448ebab5565a3
dzoliana <dzolicana@gmail.com>
2023-05-11 15:15:15 +0200

    Add NewTodo props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
+  const onTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={onTodoHandler} />
       <Todos items={todos} />
     </div>
   );
