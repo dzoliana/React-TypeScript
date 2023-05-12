c1541adcb679fa360e926767ccc51e62b5ec4cd0
dzoliana <dzolicana@gmail.com>
2023-05-12 15:06:04 +0200

    Adding styling

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
+import classes from "./TodoItem.module.css";
 import { PropsWithChildren } from "react";
 
 const TodoItem: React.FC<PropsWithChildren<{ text: string }>> = (props) => {
-  return <li>{props.text}</li>;
+  return <li className={classes.item}>{props.text}</li>;
 };
 
 export default TodoItem;
