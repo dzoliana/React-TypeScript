124a0a8a8919bd038e6ab8d3ed9cdcb1ddafd566
dzoliana <dzolicana@gmail.com>
2023-05-19 13:34:17 +0200

    Removing a todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,14 @@
 import classes from "./TodoItem.module.css";
 import { PropsWithChildren } from "react";
 
-const TodoItem: React.FC<PropsWithChildren<{ text: string }>> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<
+  PropsWithChildren<{ text: string; onRemoveTodo: () => void }>
+> = (props) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,11 +3,17 @@ import { PropsWithChildren } from "react";
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 
-const Todos: React.FC<PropsWithChildren<{ items: Todo[] }>> = (props) => {
+const Todos: React.FC<
+  PropsWithChildren<{ items: Todo[]; onRemoveTodo: (id: string) => void }>
+> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
